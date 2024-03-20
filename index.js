221bdaf8c640b2b33bf73418af18963e020c094c
aaronbannin <6948336+aaronbannin@users.noreply.github.com>
2024-03-20 11:16:46 -0700

    fix reflection

--- index.js
+++ index.js
@@ -6,8 +6,8 @@
 
 const winston = require('winston');
 
-
-const format = process.env.NODE_ENV.toLowerCase().includes("prod") ? winston.format.json() : winston.format.cli()
+const node_env = process.env.NODE_ENV ?? "";
+const format = node_env.toLowerCase().includes("prod") ? winston.format.json() : winston.format.cli();
 const logger = winston.createLogger({
     level: 'info',
     format: format,
